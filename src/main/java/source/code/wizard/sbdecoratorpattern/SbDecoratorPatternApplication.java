8bced5c70f17e18a59188fdf1ac193818df9b9d5
SourceCodeWizard <chalassproduction@gmail.com>
2024-12-30 05:35:04 +1200

    Initial commit ( master )

---
+++ src/main/java/source/code/wizard/sbdecoratorpattern/SbDecoratorPatternApplication.java
@@ -0,0 +1,13 @@
+package source.code.wizard.sbdecoratorpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDecoratorPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDecoratorPatternApplication.class, args);
+    }
+
+}
